b4b7d2e7052892b64a578e5b61a1ebe72afba747
Gloria <mcalping@ccsdbraves.org>
2025-04-24 19:00:27 -0400

    made  system check, no workie yet :((((

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,7 +13,9 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Commands;
 /**
  * This sample program shows how to control a motor using a joystick. In the operator control part
  * of the program, the joystick is read and the value is written to the motor.
@@ -28,7 +30,7 @@ public class Robot extends TimedRobot {
   private static final double MAX_SPEED = 0.25;
   private static final double MIN_SPEED = -0.25;
 
-  private static final int kMotorPort = 15;
+  private static final int kMotorPort = 13;
   private static final int kJoystickPort = 0;
 
   private final SparkFlex m_motor;
@@ -40,6 +42,9 @@ public class Robot extends TimedRobot {
     m_motor = new SparkFlex(kMotorPort, MotorType.kBrushless);
     m_joystick = new XboxController(kJoystickPort);
     m_encoder = m_motor.getEncoder();
+    SmartDashboard.putData("Check", systemCheckCommand());
+
+
   }
 
   /*
@@ -52,13 +57,37 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Joystick", m_joystick.getLeftX());
     SmartDashboard.putNumber("Motor", m_motor.getAppliedOutput());
   }
-
+  public void registerSystemCheckWithSmartDashboard() {
+    Command systemCheck = systemCheckCommand();
+  
+  }
   /** The teleop periodic function is called every control packet in teleop. */
   @Override
   public void teleopPeriodic() {
-    double voltagePercentage = m_joystick.getLeftX();
+    double voltagePercentage = MathUtil.applyDeadband(m_joystick.getLeftX(), .1);
     voltagePercentage = MathUtil.clamp(voltagePercentage, MIN_SPEED, MAX_SPEED);
 
-    m_motor.set(m_joystick.getLeftX());
+  m_motor.set(m_joystick.getLeftX());
   }
-}
+  public void runMotor() {
+    m_motor.set(.1);
+  }
+  public void stopMotor() {
+    m_motor.set(0.0);
+  }
+  protected Command systemCheckCommand() {
+    return Commands.sequence(
+        Commands.runOnce(
+            () -> {
+                runMotor(); // Start the motor
+            }
+        ),
+        Commands.waitSeconds(3.0), // Wait for 3 seconds
+        Commands.runOnce(
+            () -> {
+                stopMotor(); // Stop the motor
+            }
+        )
+    );
+          }}
+          
